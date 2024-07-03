dfe93f064ce876f820bdd32b8125b30ab82805e1
PrinzEugen39 <habibsan01@gmail.com>
2024-07-03 15:54:54 +0700

    hehe

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,8 @@
 module.exports = {
-  apps: [
-    {
-      name: "Prinz GPT",
+  apps: [{
+      name: "Prinz Overflow",
       script: "npm",
-      args: "start -- -p 3901",
+      args: "start -- -p 3902",
       watch: true,
-    },
-  ],
-
-  deploy: {
-    production: {
-      user: "satori",
-      host: "103.253.145.125",
-      ref: "origin/main",
-      repo: "git@github.com:PrinzEugen39/PrinzGPT.git",
-      path: "/home/satori/prinz-gpt",
-      "pre-deploy-local": "",
-      "post-deploy":
-        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
-      "ssh_options": "ForwardAgent=yes"
-    },
-  },
-};
+  }]
+}
